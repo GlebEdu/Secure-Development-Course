e8eb86bc0da782430a25f47bd731fe273f4778ac
GlebEdu <gvgarbuz@edu.hse.ru>
2025-10-03 00:02:56 +0300

    Вынос Pydantic схем в отдельный модуль

--- app/main.py
+++ app/main.py
@@ -1,45 +1,17 @@
-from datetime import date
 from typing import List
 
 from fastapi import FastAPI, HTTPException, Request
 from fastapi.responses import JSONResponse
-from pydantic import BaseModel, Field
+
+from .schemas import (
+    CheckinCreate,
+    CheckinResponse,
+    HabitCreate,
+    HabitResponse,
+    StatsResponse,
+)
 
 app = FastAPI(title="Habit Tracker App", version="0.1.0")
-
-
-class HabitCreate(BaseModel):
-    name: str = Field(
-        ..., min_length=1, max_length=100, description="Название привычки"
-    )
-    periodicity: int = Field(..., gt=0, description="Периодичность (в днях)")
-
-
-class HabitResponse(BaseModel):
-    id: int
-    name: str
-    periodicity: int
-    user_id: int
-
-
-class CheckinCreate(BaseModel):
-    habit_id: int = Field(..., gt=0, description="ID привычки")
-    checkin_date: date = Field(..., description="Дата отметки")
-    completed: bool = Field(..., description="Выполнено (да/нет)")
-
-
-class CheckinResponse(BaseModel):
-    id: int
-    habit_id: int
-    checkin_date: date
-    completed: bool
-
-
-class StatsResponse(BaseModel):
-    total_habits: int
-    total_checkins: int
-    completed_checkins: int
-    completion_rate: float
 
 
 class ApiError(Exception):
